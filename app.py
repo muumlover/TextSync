cbeb6dd167a5b7f585cea659e4bfd965f5cae69a
muumlover <muumlover@live.com>
2019-05-23 15:27:24 +0800

    ui load

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import asyncio
+import copy
 import hashlib  # 导入功能模块，此模块有MD5,SHA1,SHA256等方法
 
 import aiohttp_jinja2
@@ -14,7 +15,7 @@ sync_group = {
 
 async def text_sync(request):
     sync_id = request.match_info['uuid']
-    loop = request.app.loop
+    # loop = request.app.loop
     async with sse_response(request) as resp:
         if sync_id not in sync_group:
             sync_group[sync_id] = []
@@ -24,15 +25,21 @@ async def text_sync(request):
             # data = 'Text Sync Addr: {}'.format(datetime.now())
             # print(data)
             # await resp.send(data)
-            await asyncio.sleep(10, loop=loop)
-    return resp
+            # await asyncio.sleep(10, loop=loop)
+            await asyncio.sleep(10)
+    # return resp
 
 
 async def text_send(request):
     sync_id = request.match_info['uuid']
-    for sync in sync_group[sync_id]:
+    post_data = await request.json()
+    text = post_data['text']
+    if sync_id not in sync_group:
+        return web.HTTPNotFound()
+    sync_list = copy.copy(sync_group[sync_id])
+    for sync in sync_list:
         try:
-            await sync.send('new data')
+            await sync.send(text)
         except ConnectionResetError:
             sync_group[sync_id].remove(sync)
     return web.json_response({'code': 0})
@@ -56,5 +63,6 @@ aiohttp_jinja2.setup(app,
 app.router.add_route('GET', '/', index)
 app.router.add_route('GET', '/{uuid}', sync)
 app.router.add_route('GET', '/sync/{uuid}', text_sync)
-app.router.add_route('GET', '/send/{uuid}', text_send)
+app.router.add_route('POST', '/send/{uuid}', text_send)
+app.router.add_static('/static', './static')
 web.run_app(app, host='127.0.0.1', port=8080)
